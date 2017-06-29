ae0a1ddb7dc6090eae7465d797dac44aebb468bc
Tanaka Hayate <pinqphayate@gmail.com>
2017-06-30 01:54:43 +0900

    re-create module

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(
+    name='oddsman',
+    version='0.0.1',
+    author='PinkPhayate',
+    author_email='pinqphayate@gmail.com',
+    url='https://github.com/PinkPhayate',
+    description='this module extracts odds(rate) from hourse race in japan',
+    long_description=long_description,
+    zip_safe=False,
+    include_package_data=False,
+    packages=find_packages(),
+    install_requires=['bs4>=0.0.1', 'request>=0.0.13'],
+    tests_require=[],
+    setup_requires=[],
+)
