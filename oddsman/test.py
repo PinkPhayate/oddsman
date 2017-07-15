133708099d500e6b11d645c0a01e0cfa7bb405bc
kishi lab <kishi-mb2@kishinoMacBook-Pro.local>
2017-07-15 16:33:21 +0900

    不要モジュールの除去とテストモジュールの作成

--- oddsman/test.py
+++ oddsman/test.py
@@ -2,8 +2,14 @@ import oddsman
 
 
 ow = oddsman.OddsWatcher()
-# later_race_ids = ow.get_later_race_ids()
+# dict = ow.get_race_ids("0625")
+# print(dict)
+# later_race_ids = ow.get_later_race_ids(dict=dict)
 # print(later_race_ids)
-odds = ow.get_nearest_odds()
-print(odds)
+# odds = ow.get_nearest_odds()
+# print(odds)
 
+# history mode
+race_id = '201702010412'
+odds_dict = ow.get_race_odds(race_id)
+print(odds_dict)
