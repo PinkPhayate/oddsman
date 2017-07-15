a78d6756222cdb52f0c704cee958996043185756
kishi lab <kishi-mb2@kishinoMacBook-Pro.local>
2017-07-15 15:20:58 +0900

    バグの修正

--- oddsman/oddsman.py
+++ oddsman/oddsman.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib import request
-
+from datetime import datetime, date
 
 class OddsWatcher(object):
     def __get_request_via_get(self, url):
@@ -52,7 +52,7 @@ class OddsWatcher(object):
         df = []
         # Extract status
         title = soup.find('h1')
-        print(title.text)
+        # print(title.text)
         self.title = title.text
         # if title.text is not correct (e.g. another race), remove
         table = soup.find(class_='race_table_old nk_tb_common')
@@ -90,22 +90,81 @@ class OddsWatcher(object):
             time_df.append(times)
             dict = self.__2dict(row, times)
             yield dict
-        # return df, time_df
+
+    def __formalize(self, str):
+        return ('0'+str) if len(str) == 1 else str
+
+    def __get_today_data(self):
+        dt = datetime.now()
+        month = self.__formalize(str(dt.month))
+        day = self.__formalize(str(dt.day))
+        return month + day
+
+    def __get_now_time(self):
+        dt = datetime.now()
+        hour = self.__formalize(str(dt.hour))
+        minute = self.__formalize(str(dt.minute))
+        return hour + minute
+
+    def __to_dict(self, list):
+        dict = {}
+        for i in range(len(list)):
+            dict[i+1] = list[i]
+        return dict
 
     def get_race_odds(self, race_id):
         url = 'http://race.netkeiba.com/?pid=race_old&id=c' + str(race_id)
         source = self.__get_request_via_get(url)
         self.df = self.__scrape_race_info(source)
-        odds_list = [x[10] for x in self.df if len(x) > 0]
+        odds_list = [float(x[9]) for x in self.df if len(x) > 0]
         return odds_list
 
+    def get_race_history_odds(self, race_id):
+        url = 'http://race.netkeiba.com/?pid=race_old&id=c' + str(race_id)
+        source = self.__get_request_via_get(url)
+        self.df = self.__scrape_race_info(source)
+        odds_list = [float(x[10]) for x in self.df if len(x) > 0]
+        return odds_list
+
+    def get_race_history_sorted_odds_list(self, race_id):
+        odds_list = self.get_race_history_odds(race_id=race_id)
+        dict = self.__to_dict(odds_list)
+        return sorted(dict.items(), key=lambda x: x[1])
+
     def get_race_ids(self, date):
-        # url = 'http://race.netkeiba.com/?pid=race_list&id=c' + str(date)
-        url = 'http://race.netkeiba.com/?pid=race_list&id=p' + str(date)
+        # まだ開催されていないレースがc、開催終わってる日はp？？?
+        url = 'http://race.netkeiba.com/?pid=race_list&id=c' + str(date)
+        print('[info] request: ' + url)
         source = self.__get_request_via_get(url)
         dict = {}
         for d in self.__scrape_race_id(source):
             dict.update(d)
-        print(dict)
-        # df, time_df = self.__scrape_race_id(source)
-        # print(df, time_df)
+        return dict
+
+    def get_sorted_race_ids(self, date):
+        race_ids = self.get_race_ids(date)
+        return sorted(race_ids.items(), key=lambda x: x[0])
+
+    def get_later_race_ids(self, dict=None):
+        if dict is None:
+            today_data = self.__get_today_data()
+            dict = self.get_race_ids(today_data)
+        times_str = list(dict.keys())
+        sorted_times_list = sorted(times_str, key=lambda x: x.replace(':', ''))
+        now_time = self.__get_now_time()
+        return [x for x in sorted_times_list if int(now_time) < int(x.replace(':', '') if(':' in x) else 0)]
+
+    def get_nearest_odds(self):
+        today_data = self.__get_today_data()
+        for i in range(1,7):
+            data = str(int(today_data) + i)
+            dict = self.get_race_ids(data)
+            if 0 < len(dict):
+                break
+        else:
+            # TODO
+            print('[error] Could not find any race for a week')
+            return None
+        list = self.get_later_race_ids(dict=dict)
+        if 0 < len(list):
+            return self.get_race_odds(dict[list[0]])
