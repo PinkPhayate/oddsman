7d92945618118cfd9e509c71ba2f79506bb5fe2b
kishi lab <kishi-mb2@kishinoMacBook-Pro.local>
2017-07-15 15:13:00 +0900

    api版のモジュールのバグ修正

--- oddsman/oddsman.py
+++ oddsman/oddsman.py
@@ -106,19 +106,31 @@ class OddsWatcher(object):
         minute = self.__formalize(str(dt.minute))
         return hour + minute
 
+    def __to_dict(self, list):
+        dict = {}
+        for i in range(len(list)):
+            dict[i+1] = list[i]
+        return dict
+
     def get_race_odds(self, race_id):
         url = 'http://race.netkeiba.com/?pid=race_old&id=c' + str(race_id)
         source = self.__get_request_via_get(url)
         self.df = self.__scrape_race_info(source)
-        odds_list = [x[9] for x in self.df if len(x) > 0]
+        # odds_list = [float(x[9]) for x in self.df if len(x) > 0]
+        odds_list = [float(x[10]) for x in self.df if len(x) > 0]
         return odds_list
 
     def get_race_history_odds(self, race_id):
         url = 'http://race.netkeiba.com/?pid=race_old&id=c' + str(race_id)
         source = self.__get_request_via_get(url)
         self.df = self.__scrape_race_info(source)
-        odds_list = [x[10] for x in self.df if len(x) > 0]
+        odds_list = [float(x[10]) for x in self.df if len(x) > 0]
         return odds_list
+
+    def get_race_history_sorted_odds_list(self, race_id):
+        odds_list = self.get_race_history_odds(race_id=race_id)
+        dict = self.__to_dict(odds_list)
+        return sorted(dict.items(), key=lambda x: x[1])
 
     def get_race_ids(self, date):
         # まだ開催されていないレースがc、開催終わってる日はp？？?
@@ -141,9 +153,7 @@ class OddsWatcher(object):
         times_str = list(dict.keys())
         sorted_times_list = sorted(times_str, key=lambda x: x.replace(':', ''))
         now_time = self.__get_now_time()
-        time_list = [x for x in sorted_times_list if int(now_time) < int(x.replace(':', '') if(':' in x) else 0)]
-        return dict[time_list[0]]
-        # return [x for x in sorted_times_list if int(now_time) < int(x.replace(':', '') if(':' in x) else 0)]
+        return [x for x in sorted_times_list if int(now_time) < int(x.replace(':', '') if(':' in x) else 0)]
 
     def get_nearest_odds(self):
         today_data = self.__get_today_data()
@@ -156,6 +166,6 @@ class OddsWatcher(object):
             # TODO
             print('[error] Could not find any race for a week')
             return None
-        race_id = self.get_later_race_ids(dict=dict)
-        print(race_id)
-        return self.get_race_odds(race_id)
+        list = self.get_later_race_ids(dict=dict)
+        if 0 < len(list):
+            return self.get_race_odds(dict[list[0]])
